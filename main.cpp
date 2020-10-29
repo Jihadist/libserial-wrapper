258b747d9bf1a9b6ba2e7bbd5a72a0cad406b558
atom <tomasiche@gmail.com>
2020-10-29 16:40:07 +0300

    [*] Return on error [*] Open first virtual com port

--- main.cpp
+++ main.cpp
@@ -12,22 +12,35 @@ int main(int argc, char* argv[])
     QCoreApplication a(argc, argv);
     QList<SerialPortInfo> aval_ports = SerialPortInfo::availablePorts();
     qDebug() << aval_ports;
-
-    SerialPort port1("COM4", &a);
+    if (aval_ports.size() < 2)
+        return 0;
+    SerialPort port1(aval_ports.at(1).portName(), &a);
     qDebug("Port created");
-    int error_code = port1.open(QIODevice::ReadWrite);
+    bool error_code = port1.open(QIODevice::ReadWrite);
     if (error_code)
         qDebug("Port opened");
+    else
+        return -1;
     if (port1.setBaudRate(9600))
         qDebug("Baud rate changed");
+    else
+        return -2;
     if (port1.setDataBits(SerialPort::Data8))
         qDebug("Data bits changed");
+    else
+        return -3;
     if (port1.setParity(SerialPort::NoParity))
         qDebug("Parity changed");
+    else
+        return -4;
     if (port1.setStopBits(SerialPort::OneStop))
         qDebug("Stop bits changed");
+    else
+        return -5;
     if (port1.setFlowControl(SerialPort::NoFlowControl))
         qDebug("Flow control changed");
+    else
+        return -6;
     qDebug() << port1.openMode();
     QByteArray array; //(0x01, 0x04, 0x00, 0x01, 0x00, 0x1e, 0x21, 0xc2, '\0');
     array = QByteArrayLiteral("\x01\x04\x00\x01\x00\x1e\x21\xC2\x00");
